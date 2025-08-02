860033acf7f07c1e8c5fa8658bed37255be7c482
Irene Busah <ibusah@andrew.cmu.edu>
2025-08-02 22:09:39 +0200

    Project structure setup complete

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+main.py
+=========
+
+The main project entry point to run the entire ML pipeline
+"""
+
+
+# libraries
+from src.textSummarizer.logging import logger
+
+logger.info("Welcome to the Text Summarizer Project")
---
+++ src/textSummarizer/logging/__init__.py
@@ -0,0 +1,23 @@
+# libraries
+import os
+import sys
+import logging
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+log_dir = "logs"
+
+log_file = os.path.join(log_dir, "logging.log")
+
+os.makedirs(log_dir, exist_ok=True)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=logging_str,
+    handlers=[
+        logging.FileHandler(log_file),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logger = logging.getLogger("text-summarizer-ML-model")
+
